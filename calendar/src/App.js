78325dcf098bf1e544733e8315431f9f51849a82
carlos <carloseduardoherreracharry@gmail.com>
2023-01-28 22:52:30 -0500

    2701

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const Pao = <p>Te amo y te espero, perdon si actuo asi</p>
   return (
     <div className="App">
       <header className="App-header">
+        <h1>MpMjAPc</h1>
         <img src={logo} className="App-logo" alt="logo" />
+        {Pao}
         <p>
           Pao <code>Te amo demasiado</code> Siempre te esperare.
         </p>
